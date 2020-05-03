20670d0a51f0a4584c318715a40fa2c9ff47e135
Selbin_C_R <selbinzr@gmail.com>
2020-05-03 18:23:21 +0530

    changed to class

--- presistentQueue.js
+++ presistentQueue.js
@@ -4,39 +4,50 @@ const readStream = require('readline')
 const fileName = 'store.txt'
 const taskQueue = []
 
-const write = (data, dirPath) => {
-  taskQueue.push(data)
-  if (taskQueue.length > 9) {
-    while (taskQueue.length !== 0) {
-      writeToFile(taskQueue.shift, dirPath)
+class PresistentQueue {
+  constructor (fileName, baseDir, storeDir, delimiter) {
+    this.fileName = fileName || 'store.txt'
+    this.storeDir = storeDir
+    this.taskQueue = []
+    this.baseDir = baseDir || __dirname
+    this.delimiter = delimiter || '\r\n'
+  }
+
+  write (data) {
+    this.taskQueue.push(data)
+    if (this.taskQueue.length > 9) {
+      // queue length greater than 10 we will write to file
+      while (this.taskQueue.length !== 0) {
+        fs.appendFileSync(path.join(this.baseDir, this.fileName), taskQueue.shift() + '\r\n')
+        // write to a new store if file size greater than 1000 byte
+        if (fs.statSync(path.join(this.baseDir, fileName)).size > 1000) {
+          fs.renameSync(path.join(this.baseDir, fileName), path.join(this.baseDir, this.storeDir, `${new Date().getTime()}.txt`))
+        }
+      }
+    }
+  }
+
+  read (cb) {
+    try {
+      console.log(__dirname)
+      fs.readdir(path.join(this.baseDir, this.storeDir), (err, files) => {
+        if (err) throw err
+        files.forEach((file) => {
+          const readLine = readStream.createInterface({
+            input: fs.createReadStream(path.join(this.baseDir, this.storeDir, file)),
+            output: process.stdout,
+            console: false
+          })
+          readLine.on('line', (line) => cb(null, line))
+          readLine.on('close', () => {
+            fs.unlink(path.join(this.baseDir, this.storeDir, file), err => { if (err) throw err })
+          })
+        })
+      })
+    } catch (error) {
+      cb(error)
     }
   }
 }
 
-const writeToFile = (taskQueueData, dirPath) => {
-  fs.appendFileSync(path.join(dirPath, fileName), taskQueueData + '\r\n')
-  if (fs.statSync(path.join(__dirname, fileName)).size > 1000) {
-    fs.renameSync(path.join(__dirname, fileName), path.join(__dirname, 'bigStore/', `${new Date().getTime()}.txt`))
-  }
-}
-
-const read = (dirPath, cb) => {
-  try {
-    fs.readdir(dirPath, (err, files) => {
-      if (err) throw err
-      files.forEach((file) => {
-        const readLine = readStream.createInterface({ input: path.join(dirPath, file) })
-        readLine.on('line', (line) => {
-          cb(null, line)
-        })
-        readLine.on('close', () => {
-          fs.unlink(path.join(__dirname, 'bigStore/', file))
-        })
-      })
-    })
-  } catch (error) {
-    cb(error)
-  }
-}
-
-module.exports = { write, read }
+module.exports = PresistentQueue
